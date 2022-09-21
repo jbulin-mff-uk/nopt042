4f6c7391644e97553b4d206b7b837579fe0682c4
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2022-09-21 16:31:26 +0200

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 cd ~
 mkdir .picat
-cd picat
+cd .picat
 
 wget http://picat-lang.org/download/picat328_linux64.tar.gz
 tar -xf picat328_linux64.tar.gz
 rm picat328_linux64.tar.gz
-
-cd ~
 
 echo 'export PATH="$HOME/.picat:$PATH"' >> ~/.bashrc
 source ~/.bashrc

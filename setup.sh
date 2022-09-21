90375303e557f95145c7737ebbdf7d11d824f093
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2022-09-21 16:33:52 +0200

    More fixes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 cd ~
-mkdir .picat
-cd .picat
-
 wget http://picat-lang.org/download/picat328_linux64.tar.gz
 tar -xf picat328_linux64.tar.gz
 rm picat328_linux64.tar.gz
 
-echo 'export PATH="$HOME/.picat:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/Picat:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 pip install --user jupyter ipicat

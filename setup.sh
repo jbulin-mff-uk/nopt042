5302cb4d45934e39a0cae9d39d557efeec5702fc
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2022-09-21 16:27:37 +0200

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+cd ~
+mkdir .picat
+cd picat
+
+wget http://picat-lang.org/download/picat328_linux64.tar.gz
+tar -xf picat328_linux64.tar.gz
+rm picat328_linux64.tar.gz
+
+cd ~
+
+echo 'export PATH="$HOME/.picat:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+pip install --user jupyter ipicat

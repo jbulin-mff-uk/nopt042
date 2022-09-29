57a03f9460908e5351a7aa1dbeec5c667ed9e5a0
Jakub Bulín <jakub.bulin@gmail.com>
2022-09-29 16:19:14 +0200

    Prepare before class

---
+++ tutorial1/exercises/count_occurences/test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+picat count-occurences.pi [1,2,4,2,3,2] 2
+picat count-occurences.pi [1,2,2,1] 3
+picat count-occurences.pi [3,2,1] 1
---
+++ tutorial1/exercises/transpose/test.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+picat transpose.pi "{{1,2,3},{4,5,6}}"

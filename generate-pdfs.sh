f978d689a48ee5093dd3741dd5333f21cc10469f
Jakub Bulín <jakub.bulin@gmail.com>
2022-10-13 21:20:46 +0200

    Prepare tutorial 3

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
-for i in {1..12}
-do
-    jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
-    jupyter nbconvert --to webpdf tutorial$i/tutorial$i.ipynb
-done
+if [ "$*" == "" ];
+then
+    for i in {1..12}
+    do
+        jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to webpdf tutorial$i/tutorial$i.ipynb
+    done
+else
+    for i in $*; do 
+        jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to webpdf tutorial$i/tutorial$i.ipynb
+    done
+
+fi

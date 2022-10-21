8c8a3e80784be7a5cfac7fee6cf53e1c8c340919
Bulín Jakub RNDr. Ph.D <311-bulij5am@users.noreply.localhost>
2022-10-21 15:06:54 +0200

    Magic square

---
+++ tutorial3/__tmp.sol
@@ -0,0 +1,56 @@
+Problem:    
+Rows:       23
+Columns:    9 (9 integer, 0 binary)
+Non-zeros:  41
+Status:     INTEGER OPTIMAL
+Objective:  obj = 90 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 r.4                         0           -10               
+     2 r.5                         0             0               
+     3 r.6                         0             0             = 
+     4 r.7                        -3           -10               
+     5 r.8                         3             0               
+     6 r.9                         0             0             = 
+     7 r.10                        0             0             = 
+     8 r.11                       -1           -10               
+     9 r.12                        1             0               
+    10 r.13                        0             0             = 
+    11 r.14                        0             0             = 
+    12 r.15                        0           -10               
+    13 r.16                        0             0               
+    14 r.17                      -90         -1800               
+    15 r.18                       90             0               
+    16 r.19                      -13          -120               
+    17 r.20                       13             8               
+    18 r.21                      -10          -120               
+    19 r.22                       10            10               
+    20 r.23                       -6           -50               
+    21 r.24                        6             6               
+    22 r.25                     -750        -12500               
+    23 r.26                      750           500               
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 X4           *             90             0          1800 
+     2 X0           *              0             0            10 
+     3 X1           *              3             0            10 
+     4 X2           *              1             0            10 
+     5 X3           *              0             0            10 
+     6 X6           *             10            10           120 
+     7 X7           *              6             6            50 
+     8 X8           *            750           500         12500 
+     9 X5           *             13             8           120 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output

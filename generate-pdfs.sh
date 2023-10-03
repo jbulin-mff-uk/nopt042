e1d562f07d57099b752a19c342d623a9dc9fdf61
Jakub Bulín <jakub.bulin@gmail.com>
2023-10-03 10:20:57 +0200

    Generate PDFs

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -4,12 +4,12 @@ then
     for i in {1..12}
     do
         jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
-        jupyter nbconvert --to webpdf tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to webpdf --allow-chromium-download tutorial$i/tutorial$i.ipynb 
     done
 else
     for i in $*; do 
         jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
-        jupyter nbconvert --to webpdf tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to webpdf --allow-chromium-download tutorial$i/tutorial$i.ipynb
     done
 
 fi

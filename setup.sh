98b82772fa2b050f50f36900593bb60b3cb0235b
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2024-10-01 18:13:49 +0200

    Update tutorial1

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 cd ~
-wget http://picat-lang.org/download/picat328_linux64.tar.gz
-tar -xf picat328_linux64.tar.gz
-rm picat328_linux64.tar.gz
+wget http://picat-lang.org/download/picat37_linux64.tar.gz
+tar -xf picat37_linux64.tar.gz
+rm picat37_linux64.tar.gz
 
 echo 'export PATH="$HOME/Picat:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
+
 pip install --user jupyter ipicat RISE
 
+# conda install -c conda-forge jupyterlab

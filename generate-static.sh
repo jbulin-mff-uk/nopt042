85bab9111af39258d01079e842a6251230d9ab73
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2024-10-02 14:53:55 +0200

    Static html

--- generate-static.sh
+++ generate-static.sh
@@ -4,12 +4,15 @@ then
     for i in {1..12}
     do
         jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
-        jupyter nbconvert --to webpdf --allow-chromium-download tutorial$i/tutorial$i.ipynb 
+        jupyter nbconvert --to webpdf --allow-chromium-download tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to slides tutorial$i/tutorial$i.ipynb
+
     done
 else
     for i in $*; do 
         jupyter nbconvert --to notebook --inplace --execute tutorial$i/tutorial$i.ipynb
         jupyter nbconvert --to webpdf --allow-chromium-download tutorial$i/tutorial$i.ipynb
+        jupyter nbconvert --to slides tutorial$i/tutorial$i.ipynb
     done
 
 fi

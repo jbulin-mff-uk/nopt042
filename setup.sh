e4c0164b957c3b22c1b29a4f4c698104f07ef429
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2024-10-02 19:03:05 +0200

    Hide Picat

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ cd ~
 wget http://picat-lang.org/download/picat37_linux64.tar.gz
 tar -xf picat37_linux64.tar.gz
 rm picat37_linux64.tar.gz
+mv Picat .picat
 
-echo 'export PATH="$HOME/Picat:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.picat:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 pip install --user jupyter ipicat RISE

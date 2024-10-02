a402874828b58625e561037d2fc3744f49cefd20
Jakub Bulín <jakub.bulin@mff.cuni.cz>
2024-10-02 18:50:13 +0200

    Add binder start script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 cd ~
 wget http://picat-lang.org/download/picat37_linux64.tar.gz
 tar -xf picat37_linux64.tar.gz
@@ -6,7 +6,6 @@ rm picat37_linux64.tar.gz
 
 echo 'export PATH="$HOME/Picat:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-
 
 pip install --user jupyter ipicat RISE
 

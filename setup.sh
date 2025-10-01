139a6f457a0ed460da950aea3ecc320698771df7
Jakub Bulín <jakub.bulin@gmail.com>
2025-10-01 13:58:34 +0200

    Fix setup for LL

--- setup.sh
+++ setup.sh
@@ -1,25 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/.miniconda3
 rm miniconda.sh
 $HOME/.miniconda3/bin/conda init bash
 source $HOME/.miniconda3/etc/profile.d/conda.sh
-source $HOME/.bashrc
 
-conda config --set auto_activate false
-conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
-conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+# Accept terms of service
+$HOME/.miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+$HOME/.miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 
-conda create -y -n nopt042 python=3.10 -c conda-forge --override-channels
+# Create environment
+$HOME/.miniconda3/bin/conda create -y -n nopt042 python=3.10 -c conda-forge --override-channels
+
+# Activate environment
+source $HOME/.miniconda3/etc/profile.d/conda.sh
 conda activate nopt042
 
+# Install packages
 conda install -y -c conda-forge --override-channels jupyterlab=4.0.12 notebook=7.0.8 nodejs=18.15.0
 pip install mermaid-magic ipicat jupyterlab-rise nbconvert[webpdf]
 
+# Download and install Picat
 wget -q http://picat-lang.org/download/picat39_linux64.tar.gz -O picat.tar.gz
 mkdir -p $HOME/.picat
 tar -xzf picat.tar.gz -C $HOME/.picat --strip-components=1
 rm picat.tar.gz
 
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-echo 'export PATH="$HOME/.picat:$PATH"' > $CONDA_PREFIX/etc/conda/activate.d/picat.sh
+# Set up Picat path in conda environment
+mkdir -p $HOME/.miniconda3/envs/nopt042/etc/conda/activate.d
+echo 'export PATH="$HOME/.picat:$PATH"' > $HOME/.miniconda3/envs/nopt042/etc/conda/activate.d/picat.sh

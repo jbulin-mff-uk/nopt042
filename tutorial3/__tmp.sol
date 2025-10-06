a1fc9c7cff4626f86baa723062a6525c793e2d74
Jakub Bulín <jakub.bulin@gmail.com>
2025-10-06 16:44:24 +0200

    Lecture schedule update

--- tutorial3/__tmp.sol
+++ tutorial3/__tmp.sol
@@ -1,56 +1,10 @@
-Problem:    
-Rows:       23
-Columns:    9 (9 integer, 0 binary)
-Non-zeros:  41
-Status:     INTEGER OPTIMAL
-Objective:  obj = 90 (MINimum)
-
-   No.   Row name        Activity     Lower bound   Upper bound
------- ------------    ------------- ------------- -------------
-     1 r.4                       -90         -1800               
-     2 r.5                        90             0               
-     3 r.6                         0           -10               
-     4 r.7                         0             0               
-     5 r.8                         0             0             = 
-     6 r.9                        -3           -10               
-     7 r.10                        3             0               
-     8 r.11                        0             0             = 
-     9 r.12                        0             0             = 
-    10 r.13                       -1           -10               
-    11 r.14                        1             0               
-    12 r.15                        0             0             = 
-    13 r.16                        0             0             = 
-    14 r.17                        0           -10               
-    15 r.18                        0             0               
-    16 r.19                      -13          -120               
-    17 r.20                       13             8               
-    18 r.21                      -10          -120               
-    19 r.22                       10            10               
-    20 r.23                       -6           -50               
-    21 r.24                        6             6               
-    22 r.25                     -750        -12500               
-    23 r.26                      750           500               
-
-   No. Column name       Activity     Lower bound   Upper bound
------- ------------    ------------- ------------- -------------
-     1 X0           *             90             0          1800 
-     2 X1           *              0             0            10 
-     3 X2           *              3             0            10 
-     4 X3           *              1             0            10 
-     5 X4           *              0             0            10 
-     6 X6           *             10            10           120 
-     7 X7           *              6             6            50 
-     8 X8           *            750           500         12500 
-     9 X5           *             13             8           120 
-
-Integer feasibility conditions:
-
-KKT.PE: max.abs.err = 0.00e+00 on row 0
-        max.rel.err = 0.00e+00 on row 0
-        High quality
-
-KKT.PB: max.abs.err = 0.00e+00 on row 0
-        max.rel.err = 0.00e+00 on row 0
-        High quality
-
-End of output
+Optimal - objective value 90.00000000
+      0 X0                    90                       1
+      1 X1                     0                       0
+      2 X2                     3                       0
+      3 X3                     1                       0
+      4 X4                     0                       0
+      5 X6                    10                       0
+      6 X7                     6                       0
+      7 X8                   750                       0
+      8 X5                    13                       0
